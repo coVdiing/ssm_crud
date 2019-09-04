80cadfed0166c65ca28cd20e5fc245f80d16b82e
coVdiing <113714806@qq.com>
2019-09-04 12:54:08 +0800

    新增checkUser方法

--- src/main/java/com/vi/crud/service/EmployeeService.java
+++ src/main/java/com/vi/crud/service/EmployeeService.java
@@ -6,6 +6,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.vi.crud.bean.Employee;
+import com.vi.crud.bean.EmployeeExample;
+import com.vi.crud.bean.EmployeeExample.Criteria;
 import com.vi.crud.dao.EmployeeMapper;
 
 @Service
@@ -19,5 +21,18 @@ public class EmployeeService {
 	
 	public void save(Employee employee) {
 		employeeMapper.insertSelective(employee);
+	}
+	
+	/**
+	 * 校验用户名是否可用
+	 * @param empName
+	 * @return 返回true表示可用，false不可用
+	 */
+	public boolean checkUser(String empName) {
+		EmployeeExample example = new EmployeeExample();
+		Criteria criteria = example.createCriteria();
+		criteria.andEmpNameEqualTo(empName);
+		long count = employeeMapper.countByExample(example);
+		return count == 0;
 	}
 }
